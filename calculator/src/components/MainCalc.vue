407daf54b5beb14aab974a9a95b2ccb7834c887f
Percival Leonard <anrelized.kun@gmail.com>
2022-03-02 10:23:33 +0300

    12345

--- calculator/src/components/MainCalc.vue
+++ calculator/src/components/MainCalc.vue
@@ -2,8 +2,14 @@
     <div class="main-calc">
         <div class="calculator__head">
             <h2 class="calculator__head-title">«Краткосрочный займ» в Таганроге</h2>
-
-            <div class="select-group">
+            <a @click="toggleElement" class="change_office">Выберите офис</a>
+            <div v-if="isVisible" class="select-group">
+              <Select :items="Regions" />
+              <Select :items="Cities" />
+              <Select :items="Departments" />
+              <Select :items="Statuses" />
+            </div>
+            <div class="select-group-desktop">
               <Select :items="Regions" />
               <Select :items="Cities" />
               <Select :items="Departments" />
@@ -27,6 +33,7 @@ export default {
   },
   data() {
     return {
+      isVisible : false,
       Regions: [
         { value: 'Значение', text: 'Ростовская область' },
         { value: 'Значение2', text: 'Краснодарский край' }
@@ -41,6 +48,11 @@ export default {
       Statuses: [
         { value: 'Значение', text: 'Статус клиента' },
       ],
+    }
+  },
+  methods: {
+    toggleElement() {
+      this.isVisible = !this.isVisible
     }
   }
 }
@@ -70,9 +82,22 @@ select {
   padding-right: 36px;
   margin-right: 20px;
 }
+
+@media (max-width: 1024px) {
+    select {
+      width: 45%;
+      margin-bottom: 20px;
+  }
+}
+
 @media (max-width: 768px) {
   select {
     width: 45%;
+  }
+}
+@media (min-width: 426px) {
+  .change_office{
+    display: none;
   }
 }
 @media (max-width: 425px) {
@@ -80,6 +105,24 @@ select {
     width: 95%;
     margin-bottom: 20px;
     margin-right: 0;
+  }
+  .select-group{
+    margin-top: 50px;
+  }
+  .calculator__head-title{
+    margin-bottom: 50px;
+  }
+  .change_office{
+      background: #ff4533;
+      /* width: 220px; */
+      padding: 24px 27px;
+      border-radius: 100px;
+      color: #ffffff;
+      width: 100%;
+      padding: 24px 55px;
+  }
+  .select-group-desktop{
+    display: none;
   }
 }
 </style>
